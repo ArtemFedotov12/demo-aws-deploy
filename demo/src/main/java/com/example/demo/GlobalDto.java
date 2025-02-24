66e8535b3060d9d31ba599ccbce96498e839e941
ArtemFedotov12 <artemfedotov045@gmail.com>
2025-02-24 18:44:43 +0200

    check commit

--- demo/src/main/java/com/example/demo/GlobalDto.java
+++ demo/src/main/java/com/example/demo/GlobalDto.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.valiator.Custom;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +8,5 @@ import lombok.Setter;
 @Getter
 @Setter
 public class GlobalDto {
-    @Custom
     private String name;
 }
